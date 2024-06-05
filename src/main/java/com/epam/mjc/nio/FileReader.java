6b6554ac8647224e047953ef897f8adc6d51e4b4
Нодир Асомов <nasomov93@gmail.com>
2024-06-05 10:54:00 +0500

    niofile2

--- src/main/java/com/epam/mjc/nio/FileReader.java
+++ src/main/java/com/epam/mjc/nio/FileReader.java
@@ -1,6 +1,6 @@
 package com.epam.mjc.nio;
 
-import org.antlr.v4.runtime.misc.NotNull;
+import org.jetbrains.annotations.NotNull;
 
 import java.io.File;
 import java.io.IOException;
@@ -14,12 +14,12 @@ import java.util.List;
 public class FileReader {
 
     public Profile getDataFromFile(File file) {
-        String[] words = getImportant(fileToString(file));
 
+        String[] words = getImportant(fileToString(file));
         return new Profile(words[0], Integer.parseInt(words[1]), words[2], Long.parseLong(words[3]));
     }
 
-    private List<String> fileToString(File file) {
+    private List<String> fileToString(@NotNull File file) {
         Path path = Paths.get(file.getAbsolutePath());
         java.util.List<String> lines = null;
 
@@ -27,13 +27,17 @@ public class FileReader {
             lines = Files.readAllLines(path, Charset.defaultCharset());
         } catch (IOException ex) {
             ex.printStackTrace();
+
+
         }
 
         return lines;
+
     }
 
 
-    private String[] getImportant(List<String> list) {
+
+    private String [] @NotNull  getImportant( @NotNull List<String> list) {
         String[] words = new String[4];
 
         words[0] = list.get(0).substring(list.get(0).indexOf(':') + 2);
